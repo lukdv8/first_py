15e5c3608f629043009efb02ab61b19c1d098e67
Lucas Duarte <lukvig@hotmail.com>
2024-10-13 20:06:35 -0300

    calcula media

---
+++ user-input.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Digite a nota da Avaliação 1: "))
+nota2 = float(input("Digite a nota da Avaliação 2: "))
+nota3 = float(input("Digite a nota da Avaliação 3: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("\nNota da Avaliação 1: ", nota1)
+print("Nota da Avaliação 2: ", nota2)
+print("Nota da Avaliação 3: ", nota3)
+print("\nMédia das notas: ", media)
