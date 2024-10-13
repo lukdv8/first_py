4d4850a8502ce69794d47742d5285e66e985a8be
Lucas Duarte <lukvig@hotmail.com>
2024-10-13 19:44:47 -0300

    operadores logicos

---
+++ operadores.py
@@ -0,0 +1,10 @@
+A = 10
+B = 30
+C = 40
+
+#retorna falso
+print("A == B AND B > C : ", A == B and B > C)
+#retorna verdadeiro
+print("A < B OR B > C : ", A < B or B > C)
+#retorna verdadeiro
+print("NOT A == B : ", not A == B)
