e0b2126f3914fd5a6f2b60f2b6fd8824434b9d66
Lucas Duarte <lukvig@hotmail.com>
2024-10-19 09:54:57 -0300

    (conditional-exp) expressoes condicionais

---
+++ conditional-exp.py
@@ -0,0 +1,18 @@
+#expressões condicionais em python: o operador ternário de outras linguagens
+# X if condicao else Y
+
+idade = 17
+numero = 7
+a = 7
+b = 6
+tem_energia = False
+agua = "fria"
+
+#resultado = "Você é maior de idade." if idade >= 18 else "Você é menor de idade."
+#resultado = "Par" if numero % 2 == 0 else "Ímpar"
+#max_num = a if a > b else b
+#tipo_banho = "Você vai tomar um banho quente." if tem_energia else "Você vai tomar um banho frio."
+ovo = "Cozinhou." if agua == "fervida" else "O ovo está cru."
+
+
+print(ovo)
