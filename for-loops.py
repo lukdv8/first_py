7f8a7f6aaf2479b0d35e5151113f214f350d7d78
Lucas Duarte <lukvig@hotmail.com>
2025-01-01 22:02:55 -0300

    for loops

---
+++ for-loops.py
@@ -0,0 +1,14 @@
+for index in reversed(range(1, 11)):
+    if index == 9:
+        continue
+    if index == 3:
+        break
+    
+    print(index)
+    
+print()
+
+phoneNumber = "0123-4567-8901-2345"
+
+for i in phoneNumber:
+    print(i)
